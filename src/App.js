bd368236f55a39dd8caea9a31846ead67dedba49
balaji1610 <balajiece1610@gmail.com>
2023-06-19 20:00:10 +0530

    Install_Material_UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Button variant="contained" color="primary">
+        HELLO
+      </Button>
     </div>
   );
 }
